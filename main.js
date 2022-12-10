c0797c67eff6fa7f5c35af26acbd17f62a091979
test <eg>
2022-12-10 20:45:14 +0100

    R-fertigte CSS vom 03.12.22 R-Elemente (suits, wert, deck) deklarieren und Funktion deckErstellen definieren! R- html title anpassen + Fehler beseitigen R- updateDeck function erstellen! R - End-function überprüfen, in html id="Spieler" zu "players" geändert! Rohullah(R)- einfügen von Codes, die Alexander geschickt hat R- überprüfen, ob die neuen Codes verknüpft sind. R- Problem gelöst + Reihenfolge der Code sinnvoll geordnet.

--- main.js
+++ main.js
@@ -1,112 +1,25 @@
 let suits = ["Pik", "Herz", "Karo", "Kreuz"];
-let wert = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let werte = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let deck = new Array();
 let players = new Array();
 let currentPlayer = 0;
 
 function deckErstellen() {
     deck = new Array();
-    for (let i = 0 ; i < wert.length; i++)
+    for (let i = 0 ; i < werte.length; i++)
     {
         for(let x = 0; x < suits.length; x++)
         {
-            let weight = parseInt(wert[i]);
-            if (wert[i] === "J" || wert[i] === "Q" || wert[i] === "K")
+            let weight = parseInt(werte[i]);
+            if (werte[i] === "J" || werte[i] === "Q" || werte[i] === "K")
                 weight = 10;
-            if (wert[i] === "A")
+            if (werte[i] === "A")
                 weight = 11;
-            let card = { Value: wert[i], Suit: suits[x], Weight: weight };
+            let card = { Value: werte[i], Suit: suits[x], Weight: weight };
             deck.push(card);
         }
     }
 }
-
-function händeAusteilen()
-{
-    // abwechselndes Austeilen von Karten an jeden Spieler
-    // jeweils 2 Karten
-    for(let i = 0; i < 2; i++)
-    {
-        for (let x = 0; x < players.length; x++)
-        {
-            let card = deck.pop();
-            players[x].Hand.push(card);
-            renderCard(card, x);
-            updatePoints();
-        }
-    }
-
-    updateDeck();
-}
-
-let currentPlayer = 0;
-function hitMe()
-{
-    // pop eine Karte aus dem Stapel an den aktuellen Spieler.
-    // überprüfen, ob die neuen Punkte des aktuellen Spielers über 21 liegen.
-    let card = deck.pop();
-    players[currentPlayer].Hand.push(card);
-    renderCard(card, currentPlayer);
-    updatePoints();
-    updateDeck();
-    check();
-}
-
-function bleiben()
-{
-    // weiter zum nächsten Spieler, falls vorhanden ist...
-    if (currentPlayer !== players.length-1) {
-        document.getElementById('Spieler' + currentPlayer).classList.remove('active');
-        currentPlayer += 1;
-        document.getElementById('Spieler' + currentPlayer).classList.add('active');
-    }
-
-    else {
-        end();
-    }
-}
-
-function end()
-{
-    let Gewinner = -1;
-    let score = 0;
-
-    for(let i = 0; i < players.length; i++)
-    {
-        if (players[i].Points > score && players[i].Points < 22)
-        {
-            Gewinner = i;
-        }
-
-        score = players[i].Points;
-    }
-
-    document.getElementById('status').innerHTML = 'Gewinner: Spieler  ' + players[Gewinner].ID;
-    document.getElementById("status").style.display = "inline-block";
-}
-
-function check()
-{
-    if (players[currentPlayer].Points > 21)
-    {
-        document.getElementById('status').innerHTML = 'players: ' + players[currentPlayer].ID + ' verloren';
-        document.getElementById('status').style.display = "inline-block";
-        end();
-    }
-}
-
-function updateDeck()
-{
-    document.getElementById('Deckzahl').innerHTML = deck.length;
-}
-
-window.addEventListener('load', function(){
-    deckErstellen();
-    shuffle();
-    createPlayers(1);
-});
-
-
 function createPlayers(num)
 {
     players = new Array();
@@ -166,8 +79,25 @@ function startblackjack()
     shuffle();
     createPlayers(2);
     createPlayersUI();
-    händeAusteilen();
+    haendeAusteilen();
     document.getElementById('player_' + currentPlayer).classList.add('active');
+}
+function haendeAusteilen()
+{
+    // abwechselndes Austeilen von Karten an jeden Spieler
+    // jeweils 2 Karten
+    for(let i = 0; i < 2; i++)
+    {
+        for (let x = 0; x < players.length; x++)
+        {
+            let card = deck.pop();
+            players[x].Hand.push(card);
+            renderCard(card, x);
+            updatePoints();
+        }
+    }
+
+    updateDeck();
 }
 
 function renderCard(card, player)
@@ -214,3 +144,72 @@ function updatePoints()
         document.getElementById('points_' + i).innerHTML = players[i].Points;
     }
 }
+
+function hitMe()
+{
+    // pop eine Karte aus dem Stapel an den aktuellen Spieler.
+    // überprüfen, ob die neuen Punkte des aktuellen Spielers über 21 liegen.
+    let card = deck.pop();
+    players[currentPlayer].Hand.push(card);
+    renderCard(card, currentPlayer);
+    updatePoints();
+    updateDeck();
+    check();
+}
+
+function bleiben()
+{
+    // weiter zum nächsten Spieler, falls vorhanden ist...
+    if (currentPlayer !== players.length-1) {
+        document.getElementById('player_' + currentPlayer).classList.remove('active');
+        currentPlayer += 1;
+        document.getElementById('player_' + currentPlayer).classList.add('active');
+    }
+
+    else {
+        end();
+    }
+}
+
+function end()
+{
+    let Gewinner = -1;
+    let score = 0;
+
+    for(let i = 0; i < players.length; i++)
+    {
+        if (players[i].Points > score && players[i].Points < 22)
+        {
+            Gewinner = i;
+        }
+
+        score = players[i].Points;
+    }
+
+    document.getElementById('status').innerHTML = 'Gewinner: Spieler  ' + players[Gewinner].ID;
+    document.getElementById("status").style.display = "inline-block";
+}
+
+function check()
+{
+    if (players[currentPlayer].Points > 21)
+    {
+        document.getElementById('status').innerHTML = 'Spieler: ' + players[currentPlayer].ID + ' verloren';
+        document.getElementById('status').style.display = "inline-block";
+        end();
+    }
+}
+
+function updateDeck()
+{
+    document.getElementById('Deckzahl').innerHTML = deck.length;
+}
+
+window.addEventListener('load', function(){
+    deckErstellen();
+    shuffle();
+    createPlayers(1);
+});
+
+
+
