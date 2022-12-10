b2ee4152a2e9d641743c7bb4f53e94bc372d4cd5
test <eg>
2022-12-10 18:49:27 +0100

    - Fertigtes CSS vom 03.12.22 - Elemente (suits, wert, deck) deklarieren und Funktion deckErstellen definieren! - html title anpassen + Fehler beseitigen

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let suits = ["Pik", "Herz", "Karo", "Kreuz"];
 let wert = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let deck = new Array();
+let players = new Array();
+let currentPlayer = 0;
 
 function deckErstellen() {
     deck = new Array();
@@ -9,12 +11,86 @@ function deckErstellen() {
         for(let x = 0; x < suits.length; x++)
         {
             let weight = parseInt(wert[i]);
-            if (wert[i] == "J" || wert[i] == "Q" || wert[i] == "K")
+            if (wert[i] === "J" || wert[i] === "Q" || wert[i] === "K")
                 weight = 10;
-            if (wert[i] == "A")
+            if (wert[i] === "A")
                 weight = 11;
-            var card = { Value: wert[i], Suit: suits[x], Weight: weight };
+            let card = { Value: wert[i], Suit: suits[x], Weight: weight };
             deck.push(card);
         }
+    }
+}
+
+function händeAusteilen()
+{
+    // abwechselndes Austeilen von Karten an jeden Spieler
+    // jeweils 2 Karten
+    for(let i = 0; i < 2; i++)
+    {
+        for (let x = 0; x < players.length; x++)
+        {
+            let card = deck.pop();
+            players[x].Hand.push(card);
+            renderCard(card, x);
+            updatePoints();
+        }
+    }
+
+    updateDeck();
+}
+
+let currentPlayer = 0;
+function hitMe()
+{
+    // pop eine Karte aus dem Stapel an den aktuellen Spieler.
+    // überprüfen, ob die neuen Punkte des aktuellen Spielers über 21 liegen.
+    let card = deck.pop();
+    players[currentPlayer].Hand.push(card);
+    renderCard(card, currentPlayer);
+    updatePoints();
+    updateDeck();
+    check();
+}
+
+function bleiben()
+{
+    // weiter zum nächsten Spieler, falls vorhanden ist...
+    if (currentPlayer !== players.length-1) {
+        document.getElementById('Spieler' + currentPlayer).classList.remove('active');
+        currentPlayer += 1;
+        document.getElementById('Spieler' + currentPlayer).classList.add('active');
+    }
+
+    else {
+        end();
+    }
+}
+
+function end()
+{
+    let Gewinner = -1;
+    let score = 0;
+
+    for(let i = 0; i < players.length; i++)
+    {
+        if (players[i].Points > score && players[i].Points < 22)
+        {
+            Gewinner = i;
+        }
+
+        score = players[i].Points;
+    }
+
+    document.getElementById('status').innerHTML = 'Gewinner: Spieler ' + players[Gewinner].ID;
+    document.getElementById("status").style.display = "inline-block";
+}
+
+function check()
+{
+    if (players[currentPlayer].Points > 21)
+    {
+        document.getElementById('status').innerHTML = 'Spieler: ' + players[currentPlayer].ID + ' LOST';
+        document.getElementById('status').style.display = "inline-block";
+        end();
     }
 }
